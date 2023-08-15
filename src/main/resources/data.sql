ef2cf71cbce5d5b119ac944f432fdf7c53c337b9
DaviM44 <davimatheus.mendonca@hotmail.com>
2023-08-15 16:47:37 -0300

    terceira aula

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO PRODUCT (price, name) VALUES (13, 'Su-27 Flanker');
+
+INSERT INTO PRODUCT (price, name) VALUES (20, 'Su-75 Checkmate');
+
+INSERT INTO PRODUCT (price, name) VALUES (10, 'Su-25 Frogfoot');
+
+INSERT INTO PRODUCT (price, name) VALUES (30, 'Su-57 Felon ÇÇ');
